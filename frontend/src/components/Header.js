aa1bb9904bfb3b0fe47c944d9bc9cc093288c722
Daniel Galiziani <danielgaliziani2@gmail.com>
2021-10-01 20:36:52 +0100

    Add Qty Select to Cart Button

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -15,12 +15,12 @@ const Header = () => {
                             <Nav className="ms-auto">
                                 <LinkContainer  to="/cart">
                                     <Nav.Link>
-                                        <i className="fas fa-shopping-cart"></i>Cart
+                                        <i className="fas fa-shopping-cart"></i>  Cart
                                     </Nav.Link>
                                 </LinkContainer>
                                 <LinkContainer  to="/login">
                                     <Nav.Link>
-                                        <i className="fas fa-user"></i>Sign In
+                                        <i className="fas fa-user"></i>  Sign In
                                     </Nav.Link>
                                 </LinkContainer>
                             </Nav>
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,7 +1,7 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
-import { Row, Col, Image, ListGroup, Card, Button, ListGroupItem } from 'react-bootstrap'
+import { Row, Col, Image, ListGroup, Card, Button, FormControl, ListGroupItem } from 'react-bootstrap'
 import Rating from '../components/Rating'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
@@ -9,18 +9,24 @@ import { listProductsDetails } from '../actions/productActions'
 
 
 
-    const ProductScreen = ({ match }) => {
-      const dispatch = useDispatch()
+const ProductScreen = ({ history, match }) => {
+    
+    const [qty, setQty] = useState(0)
 
-      const productDetails = useSelector(state => state.productDetails)
+    const dispatch = useDispatch()
 
-      const {loading, error, product } = productDetails
+    const productDetails = useSelector(state => state.productDetails)
+    const {loading, error, product } = productDetails
     
  
 
     useEffect(() => {
         dispatch(listProductsDetails(match.params.id))
     }, [dispatch, match])
+
+    const addToCartHandler = () => {
+        history.push(`/cart/${match.params.id}?qty=${qty}`)
+    }
  
 
     return (
@@ -73,8 +79,28 @@ import { listProductsDetails } from '../actions/productActions'
                             </Row>
                         </ListGroupItem>
 
+                        {product.countInStock > 0 && (
+                            <ListGroupItem>
+                                <Row>
+                                    <Col>Qty</Col>
+                                    <Col>
+                                        <FormControl as='select' value={qty} onChange={(e) => 
+                                        setQty(e.target.value)}>
+                                            {
+                                            [...Array(product.countInStock).keys()].map((x) => (
+                                                <option key={x + 1} value={x + 1}>
+                                                    {x + 1}
+                                                </option>
+                                            ))}
+                                        </FormControl>
+                                    </Col>
+                                </Row>
+                            </ListGroupItem>
+                        )}
+
                         <ListGroupItem>
                             <Button 
+                                onClick={addToCartHandler}
                                 className='btn btn-dark col-12' 
                                 type='button'
                                 disabled={product.countInStock === 0}
