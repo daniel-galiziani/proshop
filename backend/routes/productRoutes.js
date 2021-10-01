d01bc39220cf554f3891843c7058f2931e06a248
Daniel Galiziani <danielgaliziani2@gmail.com>
2021-10-01 13:01:40 +0100

    Add Middleware and errorHandler

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -20,12 +20,14 @@ router.get(
     '/:id', 
     asyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id)
+    
     if(product) {
         res.json(product)
     } else {
-        res.status(404).json({ message: 'Product Not Found' })
+        throw new Error('Product Not Found')
     }
+})
+)
     
-}))
 
 export default router
