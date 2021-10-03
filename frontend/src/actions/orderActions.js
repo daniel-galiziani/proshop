25fd6e8e05c851b410d6454212fdcde975264fda
Daniel Galiziani <danielgaliziani2@gmail.com>
2021-10-03 20:23:10 +0100

    Add Order Details Reducer and Actions

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -2,7 +2,10 @@ import axios from 'axios'
 import {
     ORDER_CREATE_REQUEST,
     ORDER_CREATE_SUCCESS,
-    ORDER_CREATE_FAIL
+    ORDER_CREATE_FAIL,
+    ORDER_DETAILS_REQUEST,
+    ORDER_DETAILS_SUCCESS,
+    ORDER_DETAILS_FAIL
 }  from '../constants/orderConstants'
 
 
@@ -22,7 +25,7 @@ export const createOrder = (order) => async (dispatch, getState) => {
         }
       }
   
-      const {data} = await axios.post(`/api/orders`, order,  config)
+      const { data } = await axios.post(`/api/orders`, order,  config)
       
       dispatch({
         type: ORDER_CREATE_SUCCESS,
@@ -32,6 +35,41 @@ export const createOrder = (order) => async (dispatch, getState) => {
     } catch(error) {
       dispatch({
         type: ORDER_CREATE_FAIL,
+        payload: error.message && error.message.data ?
+        error.response.data.message
+        : error.message
+      })
+  
+    }
+  
+  }
+
+
+
+export const getOrderDetails = (id) => async (dispatch, getState) => {
+    try {
+      dispatch({
+        type: ORDER_DETAILS_REQUEST
+      })
+  
+      const {userLogin:{userInfo}} = getState()
+  
+      const config = {
+        headers: {
+          Authorization: `Bearer ${userInfo.token}`
+        }
+      }
+  
+      const { data } = await axios.get(`/api/orders${id}`, config)
+      
+      dispatch({
+        type: ORDER_DETAILS_SUCCESS,
+        payload: data
+      })
+
+    } catch(error) {
+      dispatch({
+        type: ORDER_DETAILS_FAIL,
         payload: error.message && error.message.data ?
         error.response.data.message
         : error.message
