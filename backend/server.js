88e9a61c7207554eb07cf65c8887971ae06537e8
Daniel Galiziani <danielgaliziani2@gmail.com>
2021-10-03 19:34:11 +0100

    Add Create Order

--- backend/server.js
+++ backend/server.js
@@ -22,8 +22,8 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/products', productRoutes)
-app.use('/api/users',userRoutes)
-app.use('/api/orders',orderRoutes)
+app.use('/api/users', userRoutes)
+app.use('/api/orders', orderRoutes)
 
 
 app.use(notFound)
