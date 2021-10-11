0ea9d45a9c413f8a2d1218f45016a1c357a64c18
Daniel Galiziani <danielgaliziani2@gmail.com>
2021-10-11 19:24:19 +0100

    Show Orders on Profile

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -8,7 +8,10 @@ import {
     ORDER_DETAILS_FAIL,
     ORDER_PAY_REQUEST,
     ORDER_PAY_SUCCESS,
-    ORDER_PAY_FAIL
+    ORDER_PAY_FAIL,
+    ORDER_LIST_MY_REQUEST,
+    ORDER_LIST_MY_SUCCESS,
+    ORDER_LIST_MY_FAIL
 }  from '../constants/orderConstants'
 
 
@@ -113,6 +116,36 @@ export const getOrderDetails = (id) => async (dispatch, getState) => {
           error.response && error.response.data.message
             ? error.response.data.message
             : error.message,
+      });
+    }
+  };
+
+  export const listMyOrders = () => async (dispatch, getState) => {
+    try {
+      dispatch({
+        type: ORDER_LIST_MY_REQUEST
+      });
+  
+      const {
+        userLogin: { userInfo }
+      } = getState();
+  
+      const config = {
+        headers: {
+          Authorization: `Bearer ${userInfo.token}`
+        }
+      };
+  
+      const { data } = await axios.get(`/api/orders/myorders`, config);
+  
+      dispatch({ type: ORDER_LIST_MY_SUCCESS, payload: data });
+    } catch (error) {
+      dispatch({
+        type: ORDER_LIST_MY_FAIL,
+        payload:
+          error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message
       });
     }
   };
