593cd9afcffc21d97bb32549233fb9ee1c58b378
Daniel Galiziani <danielgaliziani2@gmail.com>
2021-10-13 17:35:21 +0100

    Add Admin User List

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -14,7 +14,10 @@ import {
   USER_REGISTER_SUCCESS, 
   USER_UPDATE_PROFILE_FAIL, 
   USER_UPDATE_PROFILE_REQUEST,
-  USER_UPDATE_PROFILE_SUCCESS
+  USER_UPDATE_PROFILE_SUCCESS,
+  USER_LIST_REQUEST,
+  USER_LIST_SUCCESS,
+  USER_LIST_FAIL
 } from "../constants/userConstants"
 import { ORDER_LIST_MY_RESET } from '../constants/orderConstants'
 
@@ -160,11 +163,43 @@ export const updateUserProfile = (user) => async (dispatch, getState) => {
       payload: data
     })
 
-
-    localStorage.setItem('userInfo', JSON.stringify(data))
   } catch(err) {
     dispatch({
       type: USER_UPDATE_PROFILE_FAIL,
+      payload: err.message && err.message.data ?
+      err.response.data.message
+      : err.message
+    })
+
+  }
+
+}
+
+export const listUsers = () => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: USER_LIST_REQUEST
+    })
+
+    const {userLogin:{userInfo}} = getState()
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`
+      }
+    }
+
+    const {data} = await axios.get(`/api/users`, config)
+    
+    dispatch({
+      type: USER_LIST_SUCCESS,
+      payload: data,
+    })
+
+
+  } catch(err) {
+    dispatch({
+      type: USER_LIST_FAIL,
       payload: err.message && err.message.data ?
       err.response.data.message
       : err.message
