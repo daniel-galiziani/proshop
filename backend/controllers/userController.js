45c0291215037391991b7f77c03531b858dbfff8
Daniel Galiziani <danielgaliziani2@gmail.com>
2021-10-14 18:12:05 +0100

    Add User Edit Screen

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -172,9 +172,9 @@ const deleteUser = asyncHandler(async (req, res, ) => {
 // @route  GET /api/users/:id
 // @access Private/Admin
 const getUserById = asyncHandler(async (req, res, ) => {
-  const users = await User.findById(req.params.id).select('-password')
+  const user = await User.findById(req.params.id).select('-password')
   if (user) {
-    res.json(users)
+    res.json(user)
   } else {
     res.status(404)
     throw new Error('User Not Found')
